6debc607e69844f04f6819a4a8f2f1b3618b3125
Naoki Uchida <106541486+naomam@users.noreply.github.com>
2022-06-08 16:56:07 +0900

    新規Javaプロジェクト作成+課題提出

---
+++ src/assignment01.java
@@ -0,0 +1,20 @@
+
+public class assignment01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int number =1; number <= 100; number++) {
+			if (number % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (number % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(number);
+			}
+		}
+		
+	}
+
+}
